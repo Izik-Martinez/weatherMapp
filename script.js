981d9e66533f70ed5f884969f0e7d2e57f5205da
Izik-Martinez <izikma17@yahoo.com>
2022-05-13 18:14:14 -0400

    Preparing request to weatherAPI

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const detailsBox = document.querySelector(".detailsBox");
 document.addEventListener("mouseover", (e) => {
     if (e.target.tagName == "path") {
+        let pathID = e.target.id;
+        if (pathID == "AL"){
+           let cityName = "Montgomery"
+        }
+        
         detailsBox.innerText = "Testing";
         detailsBox.style.opacity = "100%";
     } else {
@@ -14,3 +19,11 @@ window.onmouseover = (e) => {
     detailsBox.style.top = (y + 20) + "px";
     detailsBox.style.left = (x) + "px";
 };
+
+// const getWeather = async () => {
+//     try {
+//         let cityName = "boston";
+//         const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9b5e48a248a824d83fdcdf5f648e6474`)
+//         console.log(result);
+//     } catch {alert("error")}
+// }
